643b6450e4538e5848c35ecdac6e5ec47a130e29
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-01-30 18:35:40 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace VariableScope_Adam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Scope
+
+            string name = "Dylan";
+
+            Console.WriteLine("name!"); // Print Dylan
+
+        }
+        if(true)
+        { // 
+         // Declared my ifScope variable INSIDE of my if code block
+         string ifScope = "Declared In our if";
+        }
+    
+    }
+}
+
